16d4b0f7625d9d483c117b1c24bf35819ab9ca9d
roadelou <roadelou@delltbr>
2020-04-16 18:47:01 +0200

    Implemented maj function

--- ci.py
+++ ci.py
@@ -5,7 +5,7 @@ from subprocess import run
 
 
 if __name__ == '__main__':
-    main()
+    maj()
 
 def main():
     print("---! Ceci est le script d'intégration continue.\nIl n'est pas encore prêt à être utilisé.")
@@ -17,7 +17,19 @@ def identifiant():
     pass
 
 def maj():
-    pass
+    branch = ""
+    with open(".identifiant", 'r') as branchFile:
+        branch = branchFile.read();
+    gitProcess = run(f"git checkout {branch}", shell=True)
+    if gitProcess.returncode != 0:
+        print("---X git n'a pas pu être lancé correctement.\nEtes-vous certain que git est bien accessible ?", file=sys.stderr)
+    # else...
+    gitProcess = run(f"git pull origin master", shell=True)
+    if gitProcess.returncode != 0:
+        print("---X Un conflit semble être apparu dans l'usage de git. Veuillez prévenir le groupe DevOps pour qu'ils puissent vous aider.", file=sys.stderr)
+    # else...
+    print("---> Mise à jour effectuée avec succès")
+    return 0
 
 def fini():
     pass
