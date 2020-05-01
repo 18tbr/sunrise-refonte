addccd0e32d1efb7847603d3fd2118d21f946770
Litao Wang <leo_wang_948@outlook.com>
2020-05-01 23:52:57 +0800

    (litao) Fonction resoultion_simulation_feuille dans Simulation.py.

--- src/Simulation.py
+++ src/Simulation.py
@@ -14,3 +14,22 @@ class Simulation(object):
         self.sortie = {}
         # Indique si la simulation a fini de tourner ou non
         self.fini = False
+
+def creation_simulation(grille, tableau_simulation):
+    pass
+
+def creation_recursive(noeud, tableau_simulation, droite, gauche):
+    pass
+
+def resolution_simulation_parallele(noeud, tableau_simulation, droite, gauche):
+    pass
+
+def resoultion_simulation_serie(noeud, tableau_simulation, droite, gauche):
+    pass
+
+# (Litao) Selons moi, cette fonction ne sert qu'à renvoyer les chiffres des résistances
+# pour faire les calculs 
+def resoultion_simulation_feuille(noeud, tableau_simulation, droite, gauche): 
+    return noeud.arg
+
+
