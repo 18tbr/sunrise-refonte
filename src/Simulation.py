82bde27eea1e5e39baf2b164932deef8426d3aa2
Litao Wang <leo_wang_948@outlook.com>
2020-05-05 22:26:07 +0800

    (litao) Initialisation de la simulation

--- src/Simulation.py
+++ src/Simulation.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.integrate import odeint
 
 
+
 class Simulation(object):
     """docstring for Simulation."""
 
@@ -16,21 +17,33 @@ class Simulation(object):
         # Indique si la simulation a fini de tourner ou non
         self.fini = False
 
-def creation_simulation(grille, tableau_simulation):
+# (Litao) Il nous faut initialisation les températures du système
+# Idée : Peut-être qu'on pourrait utiliser le Tkinter pour entrer les températures
+def initialisationTempetarue(matrice):
     pass
 
-def creation_recursive(noeud, tableau_simulation, droite, gauche):
-    pass
-
-def resolution_simulation_parallele(noeud, tableau_simulation, droite, gauche):
-    pass
-
-def resoultion_simulation_serie(noeud, tableau_simulation, droite, gauche):
-    pass
-
-# (Litao) Selons moi, cette fonction ne sert qu'à renvoyer les chiffres des résistances
-# pour faire les calculs 
-def resoultion_simulation_feuille(noeud, tableau_simulation, droite, gauche): 
-    return noeud.arg
-
+# (Litao) Selons l'équation sur la page 4 du document "mécatroComplementProjet",on enregistre
+# les données necéssaires dans une matrice autre que "tableau" pour établir le système d'EDs.
+# On néglige pour le moment l'effet des flux thermiques φ.
+def initialisationSimulation(tableau):
+    [n,m] = np.shape(tableau)
+    # n=m, car on a une matrice carrée
+    
+    nMat = n
+    mMat = 3
+    matrice = np.zeroes((nMat, mMat))
+    # Une matrice qui enregistre les Ci, Hiéquivalent et Ti de chaque maille en ligne(mMat)    
+    
+    initialisationTemperature(matrice)
+    # Initailisation de la température de chaque maille
+    
+    for i in range(0,n):
+        for j in range(0,m):
+            if i == j :
+                matrice[i][0] = tableau[i][j]
+                # le condensateur de chaque maille
+            elif tableau[i][j] != 0:
+                matrice[i][1] = (matrice[j][2]-matrice[i][2])/tableau[i][j] + matrice[i][1]
+                #Calcul de la conductance équivalente
+ return matrice
 
