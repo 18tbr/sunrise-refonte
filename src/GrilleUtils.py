c4577ffca9ed84d13674184effe57651da31cd72
Théo Dumont <theo.dumont@mines-paristech.fr>
2020-05-08 17:14:05 +0200

    (theo) Documentation pour creerImage et modifications mineures

--- src/GrilleUtils.py
+++ src/GrilleUtils.py
@@ -1,36 +1,41 @@
 import numpy as np
 
 
-def remplirZone(image,couche, NW, SE, val):
+def remplirZone(image, canal, NW, SE, val):
     """
     Colorie une zone de l'image.
 
     Parameters
     ----------
     image : numpy array
-        Image to fill.
+        Image à remplir.
     NW = (NW_h, NW_w) : (int, int)
         Coin North-West de la portion de l'image.
     SE = (SE_h, SE_w) : (int, int)
         Coin South-East de la portion de l'image.
     val : int
         Valeur avec laquelle colorier la portion d'image.
+
+    Returns
+    -------
+    La fonction modifie `image` en place.
     """
     print(f"On colorie de {NW} à {SE}")
     NW_h, NW_w = NW
     SE_h, SE_w = SE
-    image[NW_h:SE_h, NW_w:SE_w,couche ] = val
+    image[NW_h:SE_h, NW_w:SE_w, canal] = val
 
 
-def creerMultiCanal(image, racine, numRacine, NW, SE, profondeur):
+def creerImage(image, racine, numRacine, NW, SE, profondeur):
     """
-    Fonction récursive de création du canal résistance de l'arbre.
+    Fonction récursive de création de l'image représentant l'arbre.
 
     Parameters
     ----------
     image : numpy array
-        Image to modify.
-    racine : Noeud
+        Image à modifier.
+    racine : Grille.Noeud
+        Noeud que l'on examine.
     numRacine : int
         Numéro de la racine parmi les fils de son père.
     NW = (NW_h, NW_w) : (int, int)
@@ -38,11 +43,27 @@ def creerMultiCanal(image, racine, numRacine, NW, SE, profondeur):
     SE = (SE_h, SE_w) : (int, int)
         Coin South-East de la portion de l'image.
     profondeur : int
-        Profondeur de racine, utile pour savoir si l'on divise
-        verticalement ou horizontalement.
-        On alterne le sens de division, et on commence par une
-        division verticale.
+        Profondeur de racine, stocké pour colorier le 3e canal.
 
+    Returns
+    -------
+    La fonction modifie `image` en place.
+
+    Notes
+    -----
+    L'image représentant un arbre possède 3 canaux :
+    - le canal 0 contient les valeurs des résistances de l'arbre ;
+    - le canal 1 contient les valeurs des capacités de l'arbre ;
+    - le canal 2 contient les profondeurs des noeuds de l'arbre.
+
+    A chaque appel récursif, on lit le type du ``Noeud`` sur lequel on
+    tombe :
+    - si l'on tombe sur une ``Feuille``, on colorie les zones 0 et 2 ;
+    - si l'on tombe sur une liaison ``Serie``, on divise l'image
+    verticalement et on remplit le canal 1 au passage, puis on
+    réalise l'appel récursif ;
+    - si l'on tombe sur une liaison ``Parallele``, on divise l'image
+    horizontalement et on réalise l'appel récursif.
     """
     # get coord
     NW_h, NW_w = NW
@@ -76,4 +97,4 @@ def creerMultiCanal(image, racine, numRacine, NW, SE, profondeur):
             new_NW = (new_NW_h, new_NW_w)
             new_SE = (new_SE_h, new_SE_w)
             # appel résursif
-            creerMultiCanal(image, fils, num_fils, new_NW, new_SE, profondeur + 1)
+            creerImage(image, fils, num_fils, new_NW, new_SE, profondeur + 1)
