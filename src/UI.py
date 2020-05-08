f142789f2d4f1b26ca0eab49716d38973958d3b0
unknown <maud.bernat@mines-paristech.fr>
2020-05-08 14:20:42 +0200

    (maud) Ecriture du fichier UI.

--- src/UI.py
+++ src/UI.py
@@ -1,22 +1,85 @@
 import tkinter as tk
-from .Animation import *
+from tkinter import filedialog, Text
+import os
 
-class UI(object):
-    """docstring for UI."""
+root = tk.Tk()
 
-    def __init__(self):
-        super(UI, self).__init__()
-        # Un dictionnaire contenant toutes les valeurs rentrées par l'utilisateur dans l'interface et qui pourront seront utilisées pour la simulation
-        self.pUI = {}
-        # L'objet Animation que l'on voudra afficher.
-        self.animation = None
+#creation de la fenetre
+canvas = tk.Canvas(root, height=700, width=700)
+canvas.pack()
 
 
-    def lecture_parametres():
-        # Fonction responsable de lire les paramètres précédemment utilisés par l'application depuis le fichier parametres_ui.json et de les mettre dans self.pUI
-        pass
+
+#creation du frame choix des parametres
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.8, relx = 0.1, rely=0.1)
 
 
-    def sauvegarde_parametres():
-        # Sauvegarde les valeurs de self.pUI dans le fichier parametres_ui.json.
-        pass
+#creation du titre
+titre = tk.Label(frame, text = "Choix des paramètres de la simulation", fg = "white", bg = "green")
+titre.pack()
+
+
+#definition de la fonction qui ouvre les fichiers
+
+def addfichier() :
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File", filetypes=(("text", "*.text"),("all files", "*./")))
+
+
+
+#creation des trois boutons de saisie des parametres
+T_ext = tk.Button(frame, text = "Selectionner le fichier de température extérieure", fg="white", bg="gray", command=addfichier)
+T_ext.place(relwidth=0.4, relheight=0.1, relx = 0.1, rely=0.2)
+
+
+T_int = tk.Button(frame, text = "Selectionner le fichier de température intérieure", padx=10, pady=5, fg="white", bg="gray")
+T_int.place(relwidth=0.4, relheight=0.1, relx = 0.1, rely=0.4)
+
+
+Puissance_int = tk.Button(frame, text = "Selectionner le fichier de puissance intérieure", padx=10, pady=5, fg="white", bg="gray")
+Puissance_int.place(relwidth=0.4, relheight=0.1, relx = 0.1, rely=0.6)
+
+#creation des trois boutons d'affichage des courbes
+T_ext_aff = tk.Button(frame, text = "Afficher la courbe de température extérieure", fg="white", bg="gray")
+T_ext_aff.place(relwidth=0.4, relheight=0.1, relx = 0.5, rely=0.2)
+
+
+T_int_aff = tk.Button(frame, text = "Afficher la courbe de température intérieure", padx=10, pady=5, fg="white", bg="gray")
+T_int_aff.place(relwidth=0.4, relheight=0.1, relx = 0.5, rely=0.4)
+
+
+Puissance_int_aff = tk.Button(frame, text = "Afficher la courbe de puissance intérieure", padx=10, pady=5, fg="white", bg="gray")
+Puissance_int_aff.place(relwidth=0.4, relheight=0.1, relx = 0.5, rely=0.6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+############################
+root.mainloop()
