c5b5367ccb5f7b4d72bb7ed2066eb2f280276280
roadelou <tableau.roche@gmail.com>
2020-05-22 20:29:53 +0200

    (tbr) Ecriture de fonction pour récupérer rapidement les données sur lesquelles faire l'entrainement.

---
+++ src/Entrainement.py
@@ -0,0 +1,26 @@
+# Ce fichier contient un ensemble de méthodes utiles à l'entrainement des autoencodeurs.
+
+import os   # Utile pour les manipulations de fichiers
+from Genetique import Genetique
+from Outils import lireTableau, lireDossier # Utile pour récupérer les mesures
+
+def creationGenetiqueLectureDossier(dossier, Cint, taillePopulation, generationMax, objectif):
+    # Renvoie un objet génétique crée à partir des mesures trouvées dans le dossier en argument.
+    T, Tint, Text, Pint = lireDossier(dossier)
+    return Genetique(Cint, T, Tint, Text, Pint, taillePopulation, generationMax, objectif)
+
+def lectureBlob(Cint, taillePopulation, generationMax, objectif):
+    # Renvoie la liste des population crées à partir des données dans les dossiers de blob. Cette fonction doit être lancée lorsque le repértoire courant est la racine de ce dépôt git.
+    listeDossiers = os.listdir(os.path.join("blob","mesures"))
+    populations = []
+    for dossier in listeDossiers:
+        print("Récupération des mesures dans le dossier", os.path.join("blob","mesures",dossier))
+        populations.append(creationGenetiqueLectureDossier(os.path.join("blob","mesures",dossier), Cint, taillePopulation, generationMax, objectif))
+    return populations
+
+def unificationPopulation(listeGenetiques):
+    # Prend en argument une liste d'objets Génétique et renvoie le liste de toutes leurs populations concaténées. Utile pour avoir une seule liste pour l'entrainement d'un autoencodeurs.
+    populationUnifie = []
+    for objetGenetique in listeGenetiques:
+        populationUnifie += objetGenetique.population
+    return populationUnifie
---
+++ tests/script_Entrainement.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import os
+import sys
+import numpy as np
+
+# Il faut ajouter src au PYTHONPATH avant tout, sinon les modules n'auront pas accès à leurs propres imports.
+sys.path.append(f"{os.getcwd()}/src")
+
+import Coefficients
+from Genetique import Genetique
+from Entrainement import lectureBlob, unificationPopulation
+
+def test_entrainement():
+    Genetique.PROFONDEUR_MAX_ARBRE = 20
+    Genetique.LARGEUR_MAX_ARBRE = 20
+    taille = 50
+    Cint = Coefficients.muC
+    listeGenetiques = lectureBlob(Cint, taillePopulation=taille, generationMax=100, objectif=10)
+    print(len(listeGenetiques))
+    population = unificationPopulation(listeGenetiques)
+    taille = len(population)
+    print(taille)
+    scores = []
+    images = []
+    for i in range(len(population)):
+        print(f"{i}/{taille}")
+        scores.append(-population[i].score())
+        images.append(population[i].ecritureImage(largeur=100, hauteur=100))
+    plt.plot(range(taille), np.log(np.array(scores)))
+    plt.show()
+if __name__ == "__main__":
+    test_entrainement()
