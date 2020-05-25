94afb152b85d3a9567600fa2fa97d84f07e18626
Théo Dumont <theo.dumont@mines-paristech.fr>
2020-05-25 14:12:58 +0200

    (theo) Ajout de documentation pour Outils.

--- src/Outils.py
+++ src/Outils.py
@@ -12,6 +12,11 @@ def lireTableau(nomDuFichier):
     ----------
     nomDuFichier : string
         Tableau à lire.
+
+    Exceptions levées
+    -----------------
+    ValueError
+        Lorsque le fichier `nomDuFichier` n'existe pas.
     """
     baseNom, extension = os.path.splitext(nomDuFichier)
     if extension == ".npy":
@@ -25,24 +30,41 @@ def lireTableau(nomDuFichier):
 
 
 def lireDossier(nomDuDossier):
-    # Une fonction qui simplifie l'entrainement récupérant directement toutes les grandeurs d'intérêt à partir des fichiers csv contenus dans un dossier. Renvoie une erreur si les fichiers (Tint.csv, Text.csv et Pint.csv) attendus n'existent pas dans le dossier
+    """Simplifie l'entrainement en récupérant directement toutes les grandeurs
+    d'intérêt à partir des fichiers .csv contenus dans un dossier.
+
+    Paramètres
+    ----------
+    nomDuDossier : str
+        Dossier cible.
+
+    Exceptions levées
+    -----------------
+    ValueError
+        Lorsque les fichiers (Tint.csv, Text.csv et Pint.csv) attendus
+        n'existent pas dans le dossier `nomDuDossier`.
+    ValueError
+        Lorsque les différentes grandeurs données n'ont pas la même base de
+        temps, et ne peuvent donc pas être utilisées ensemble.
+    """
     fichierTint = os.path.join(nomDuDossier, "Tint.csv")
     fichierText = os.path.join(nomDuDossier, "Text.csv")
     fichierPint = os.path.join(nomDuDossier, "Pint.csv")
 
-    # On lit les différents fichiers trouvés et on les transpose pour plus de facilités
+    # On lit les différents fichiers trouvés et on les transpose pour plus de
+    # facilité.
     tableauTint = lireTableau(fichierTint).T
     tableauText = lireTableau(fichierText).T
     tableauPint = lireTableau(fichierPint).T
 
-    # On vérifie que les données ont toutes la même base de temps
+    # On vérifie que les données ont toutes la même base de temps.
     tempsTint, Tint = tableauTint[0], tableauTint[1]
     tempsText, Text = tableauText[0], tableauText[1]
     tempsPint, Pint = tableauPint[0], tableauPint[1]
 
     if (tempsTint - tempsText).any() or (tempsPint - tempsText).any():
         raise ValueError(
-            "Les différentes grnadeurs données n'ont pas la même base de temps et ne peuvent donc pas être utilisées ensemble."
+            "Les différentes grandeurs données n'ont pas la même base de temps et ne peuvent donc pas être utilisées ensemble."
         )
-    # si tout à fonctionné, on renvoie les tableaux d'intérêt
+    # si tout a fonctionné, on renvoie les tableaux d'intérêt
     return tempsTint, Tint, Text, Pint
